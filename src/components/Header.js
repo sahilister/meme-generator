126c8208794228900256c3ac2ddbd02b76a337e1
sahilister <sahil.dhiman27091999@gmail.com>
2020-01-23 22:10:06 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <div>
+            <header>
+                <h1>Meme Generator</h1>
+                <img 
+                    src="https://render.fineartamerica.com/images/rendered/small/poster/images/artworkimages/square/1/troll-face-jeffrey-wong.jpg"
+                    alt="problem?"
+                />
+            </header>
+        </div>
+    )
+}
+
+export default Header;
