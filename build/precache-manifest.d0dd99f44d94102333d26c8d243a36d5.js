653a582c4df54d25d4f07b35a39232b55e6b5da5
sahilister <sahil.dhiman27091999@gmail.com>
2020-01-25 21:03:19 +0530

    Add deploy link

---
+++ build/precache-manifest.d0dd99f44d94102333d26c8d243a36d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d019be3b5b3e16c10ebffafa27d53f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a07fc02af4789c06b54",
+    "url": "/static/css/main.b7e55ef4.chunk.css"
+  },
+  {
+    "revision": "e2fa0279e3b395d83e8f",
+    "url": "/static/js/2.6d15567d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6d15567d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a07fc02af4789c06b54",
+    "url": "/static/js/main.a34b7f58.chunk.js"
+  },
+  {
+    "revision": "12c41c615f15abc01db1",
+    "url": "/static/js/runtime-main.f271d3ce.js"
+  }
+]);
